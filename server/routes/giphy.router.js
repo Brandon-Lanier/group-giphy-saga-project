ca768a261d19ae5be54d075b6c6ed0b5cd9d7798
Brandon Lanier <brandonjlanier@gmail.com>
2022-03-03 10:56:13 -0600

    Added a giphy API request on router

---
+++ server/routes/giphy.router.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const express = require('express');
+const pool = require('../modules/pool');
+require('dotenv').config();
+
+
+
+  router.get('/:q', (req, res) => {
+    const searchTxt = req.params.q
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTxt}`)
+    .then(response => res.send(response.data))
+    .catch(error => {
+        res.sendStatus(500)
+    })
+})
+
+module.exports = router;
