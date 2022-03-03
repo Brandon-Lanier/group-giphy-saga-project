aa4a6ed8f453e404a6c0c0c61c890eba518c4779
Josh Kralewski <joshkmanj@gmail.com>
2022-03-03 13:22:50 -0600

    Basic index setup to include/integrate sagas and reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const imageList = (state = [], action =>{
+    switch(action.type){
+        case 'SET_IMAGE':
+        return action.payload
+        default:
+            return state;
+    }
+})
+function * rootSaga(){
+    yield takeEvery('FETCH_IMAGE', fetchImage)
+    
+}
+
+// ---------- GET Routes -----------------------
+function* fetchImage(){
+    const imageData = yield axios.get('/image')
+    
+}
+// ---------- END GET Routes -------------------
+
+
+
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+        //reducers go here
+        imageList
+    }),
+    // Here we add middleware to interact with code as it comes through
+    applyMiddleWare(
+        sagaMiddleware
+        , logger
+    ),
+);
+
+sagaMiddleware.run(rootSaga);
+
+
+
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
